c27b2ace16dc9fc09752276e66888bb825fb0cd8
Nicolas Butarbutar <140901398+NicolasButarbutar@users.noreply.github.com>
2026-03-27 17:28:43 +0700

    Refactor Driver1 class and update package structure

--- src/fintech/driver/Driver1.java
+++ src/fintech/driver/Driver1.java
@@ -1,15 +1,45 @@
-package academic.driver;
+package fintech.driver;
+
+import fintech.model.Account;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S24038 Nicolas J Grace Butarbutar
  */
+//OK
 public class Driver1 {
 
-    public static void main(String[] _args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Account> accounts = new ArrayList<>();
 
-        // codes
+        while (true) {
+            if (!scanner.hasNextLine()) {
+                break;
+            }
 
+            String input = scanner.nextLine().trim();
+
+            if ("---".equals(input)) {
+                break;
+            }
+
+            if (input.startsWith("create-account#")) {
+                String[] data = input.split("#");
+
+                if (data.length == 3) {
+                    Account newAccount = new Account(data[1], data[2]);
+                    accounts.add(newAccount);
+                }
+            }
+        }
+
+        for (Account acc : accounts) {
+            System.out.println(acc);
+        }
+
+        scanner.close();
     }
-
 }
